6bc732a61aeeaf2e09c4c55ac44e1fac0b24f5a1
MaksymRL <loliknight000@gmail.com>
2026-08-16 18:32:21 +0200

    Setup fix

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -111,6 +111,7 @@ module.exports = {
       interpreter: 'npx',
       interpreter_args: 'tsx',
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
